74ecd77c5207fde8aba587f952ca62d5c0378dab
Paraross <Adrago@onet.pl>
2025-01-15 11:26:02 +0100

    Implement PolyCoeffs with ToString

--- Lab4/MainWindow.xaml.cs
+++ Lab4/MainWindow.xaml.cs
@@ -46,9 +46,12 @@ namespace WpfUI
         private void Calculate_Button_Click(object sender, RoutedEventArgs e)
         {
             var rootValues = Roots.Select(root => root.Value);
-            var scale = (float)Double.Parse(ScaleText.Text);
+            var scale = float.Parse(ScaleText.Text);
             var rootsPoly = new PolyRootsScale(rootValues.ToArray(), scale);
             RootsPolyLabel.Content = rootsPoly.ToString();
+
+            var coeffPoly = new PolyCoeffs([1.0f, 2.5f, -3.1f]);
+            CoeffPolyLabel.Content = coeffPoly.ToString();
         }
 
         private void Pop_Root_Button_Click(object sender, RoutedEventArgs e)
@@ -97,6 +100,65 @@ namespace WpfUI
                     s += String.Format("(x - {0})", root.ToString());
                 }
             }
+            return s;
+        }
+    }
+
+    class PolyCoeffs
+    {
+        private List<float> coeffs;
+
+        public PolyCoeffs(List<float> coeffs)
+        {
+            this.coeffs = coeffs;
+        }
+
+        public int degree()
+        {
+            return coeffs.Count - 1;
+        }
+
+        public void increasePower()
+        {
+            // not sure if this works
+            coeffs.Add(0.0f);
+            for (var i = coeffs.Count - 1; i >= 1; i--)
+            {
+                coeffs[i] = coeffs[i - 1];
+            }
+            coeffs[0] = 0.0f;
+        }
+
+        public override string ToString()
+        {
+            var s = "";
+
+            // iterates over index, value pairs in reverse order
+            foreach (var it in coeffs.Select((x, i) => new { Value = x, Index = i }).Reverse())
+            {
+                var degree = it.Index;
+                var coeff = it.Value;
+
+                var degreeStr = degree switch
+                {
+                    1 => "x",
+                    var x when (x > 1) => String.Format("x^{0}", degree),
+                    _ => "", // var x when (x < 1)
+                };
+
+                if (degree == this.degree())
+                {
+                    s += coeff.ToString() + degreeStr;
+                }
+                else
+                {
+                    var sign = coeff > 0.0f ? '+' : '-';
+
+                    var coeffValue = float.Abs(coeff);
+                    s += String.Format(" {0} {1}{2}", sign, coeffValue, degreeStr);
+                }
+            }
+
             return s;
         }
     }
